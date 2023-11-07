23a95d8a386afe2c7dee29b041b7515758daca5d
timofei <144905033+timofei2005@users.noreply.github.com>
2023-11-07 22:36:00 +0300

    Initial commit

---
+++ paralelogram.py
@@ -0,0 +1,16 @@
+class Paralelipiped:
+    def __init__(self,length,width,height):
+        self.length = length
+        self.height = height
+        self.width = width
+
+    def area(self):
+        s = 2 *(self.width * self.length + self.width * self.height + self.length * self.height)
+        return s
+
+    def volume(self):
+        return self.height * self.length * self.width
+
+
+paralelipiped = Paralelipiped(3,2,4)
+print(paralelipiped.area(),paralelipiped.volume())
---
+++ tests.py
@@ -0,0 +1,28 @@
+import unittest
+from paralelogram import Paralelipiped
+
+class TestParalelogram(unittest.TestCase):
+
+    def test_area(self):
+        lenght = 3
+
+        whidth = 2
+
+        height = 4
+        p = Paralelipiped(lenght,whidth, height)
+        result = p.area()
+        self.assertAlmostEqual(result, 52)
+    def test_volume(self):
+        lenght = 3
+
+        whidth = 2
+
+        height = 4
+        p = Paralelipiped(lenght,whidth, height)
+        result = p.volume()
+        self.assertAlmostEqual(result, 24)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
